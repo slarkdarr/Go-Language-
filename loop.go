db531c6c55b35d65e75943e9543598b1a738bab3
Daffa Ananda Pratama Resyaly <68500220+slarkdarr@users.noreply.github.com>
2020-10-24 00:28:04 +0700

    Create loop.go

---
+++ loop.go
@@ -0,0 +1,41 @@
+// Program yang menggunakan loop
+package main
+
+import "fmt"
+
+func main() {
+
+    i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
+
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
+
+    for {
+        fmt.Println("loop")
+        break
+    }
+
+    for n := 0; n <= 5; n++ {
+        if n%2 == 0 {
+            continue
+        }
+        fmt.Println(n)
+    }
+}
+/* Akan menghasilkan:
+1
+2
+3
+7
+8
+9
+loop
+1
+3
+5
+*/
